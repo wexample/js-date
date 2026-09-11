058643c59249cc768e08e5debd71435172e06f03
weeger <romain.weeger@gmail.com>
2026-09-11 15:04:35 +0200

    Initial commit

---
+++ .wex/python/app_manager/app_workdir.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from wexample_wex_addon_dev_javascript.workdir.javascript_package_workdir import JavascriptPackageWorkdir
+
+
+class AppWorkdir(JavascriptPackageWorkdir):
+    pass
